675313e0b0cc25ce15d6f31dc506622dc943c7dc
BC46 <cees3331x00@gmail.com>
2025-09-17 22:54:43 +0200

    Add code comments and fix wrong assumption about the target view

--- TargetScreen.c
+++ TargetScreen.c
@@ -1,4 +1,5 @@
-// TODO: Describe plugin and mention 00679C18 patch FL.exe (214470, 00 -> 01), and E15D3 = tooltip Ids HUD targetedobject (hover flag as well?)
+// This is a reimplementation of the Dynamic Target Screen plugin by Venemon.
+// The plugin toggles the top-down view of the target wireframe when pressing the "Switch to Target" button.
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
@@ -8,7 +9,7 @@
 
 #define ADDR_GET_TARGET_STATUS_MINIMIZED        ((PBYTE) 0x4E2C25)
 #define ADDR_UPDATE_SWITCH_TO_TARGET_SELECTED   ((PBYTE) 0x4E2C2D)
-#define ADDR_SWITCH_TO_TARGET_BTN_MINIMIZED     ((PBYTE) 0x4E36DA)
+#define ADDR_SWITCH_TO_TARGET_BTN_HIDDEN        ((PBYTE) 0x4E36DA)
 
 DWORD dummy;
 #define ProtectX( addr, size ) \
@@ -23,38 +24,51 @@ DWORD dummy;
 
 BOOL g_targetDllLoaded;
 
+typedef enum TARGETSTATUSVIEW
+{
+    STATUSVIEW_CONTACTLIST    = 0,
+    STATUSVIEW_TARGET         = 1,
+    STATUSVIEW_FORMATION      = 2,
+    STATUSVIEW_FORCE_DWORD    = 0x7FFFFFFF
+} TARGETSTATUSVIEW;
+
 typedef struct TargetStatusHandler
 {
-    BYTE    x00[0x14C];
-    BOOLEAN windowMinimized;        // 0x14C
-    BYTE    x14D[0x177];
-    BOOL    switchToTargetSelected; // 0x2C4
+    BYTE                x00[0x14C];
+    BOOLEAN             windowOpen;     // 0x14C
+    BYTE                x14D[0x177];
+    TARGETSTATUSVIEW    selectedView;   // 0x2C4
 } TargetStatusHandler;
 
-BOOLEAN STDCALL GetTargetStatusMinimized_Hook( TargetStatusHandler* handler )
+BOOLEAN STDCALL GetTargetStatusMinimized_Hook( TargetStatusHandler* pHandler )
 {
-    if (handler->windowMinimized &&
-        handler->switchToTargetSelected != g_targetDllLoaded)
+    // When the Target Status window is minimized, the wireframe is not visible, so don't toggle the top-down Target View.
+    // If the window is open and the Target DLL is loaded, the target wireframe is always visible, so allow it to toggle.
+    // Finally, if the window is open and the DLL is not loaded, do not toggle the view if the target wireframe is visible.
+    if (pHandler->windowOpen &&
+        (g_targetDllLoaded || pHandler->selectedView == STATUSVIEW_TARGET))
     {
         TOP_DOWN_TARGET_VIEW = !TOP_DOWN_TARGET_VIEW;
     }
 
-    return handler->windowMinimized; // overwritten instruction
+    return pHandler->windowOpen; // overwritten instruction
 }
 
 void Patch()
 {
     ProtectX( ADDR_GET_TARGET_STATUS_MINIMIZED, 7 );
-    *ADDR_GET_TARGET_STATUS_MINIMIZED = 0x57;
+    *ADDR_GET_TARGET_STATUS_MINIMIZED = 0x57; // push edi
     CALL( ADDR_GET_TARGET_STATUS_MINIMIZED + 1, GetTargetStatusMinimized_Hook );
 
     if (g_targetDllLoaded = GetModuleHandle( "HudTarget.dll" ) != NULL)
     {
-        ProtectX( ADDR_UPDATE_SWITCH_TO_TARGET_SELECTED, 6 );
-        ProtectX( ADDR_SWITCH_TO_TARGET_BTN_MINIMIZED, 1 );
+        // The Adv Wide HUD plugin hides the Switch To Target button, so make it visible again.
+        ProtectX( ADDR_SWITCH_TO_TARGET_BTN_HIDDEN, 1 );
+        *ADDR_SWITCH_TO_TARGET_BTN_HIDDEN = 0x08; // or
 
-        memcpy( ADDR_UPDATE_SWITCH_TO_TARGET_SELECTED, "\x66\x0F\x1F\x44\x00\x00", 6 );
-        *ADDR_SWITCH_TO_TARGET_BTN_MINIMIZED = 0x08;
+        // When the Switch To Target button is visible, make sure FL doesn't do anything when the user presses it.
+        memcpy( ADDR_UPDATE_SWITCH_TO_TARGET_SELECTED, "\x66\x0F\x1F\x44\x00\x00", 6 ); // nop
+        ProtectX( ADDR_UPDATE_SWITCH_TO_TARGET_SELECTED, 6 );
     }
 }
 
